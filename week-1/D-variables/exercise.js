13e5b9e59ef8457b79b5a61d816e4518fdf1fd1d
Viktoryia802 <villette@list.ru>
2020-04-07 01:44:05 +0200

    completed with d exercise

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,5 @@
-// Start by creating a variable `greeting`
+var greeting = "Hello World!"; // Start by creating a variable `greeting`
 
+console.log(greeting);
+console.log(greeting);
 console.log(greeting);
