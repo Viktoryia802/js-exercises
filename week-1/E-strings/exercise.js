bc532b69ff5b5b5d7e25f20fa7ad08cd9bd86ff5
Viktoryia802 <villette@list.ru>
2020-04-07 01:53:49 +0200

    completed E-strings

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,3 +1,5 @@
-// Start by creating a variable `message`
+var message = "This is a string"; // Start by creating a variable `message`
+console.log(message);
 
+var message = "string";
 console.log(message);
