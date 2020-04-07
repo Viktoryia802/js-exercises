d2cda71a31b033dc2e154ae18e447f6feca42757
Viktoryia802 <villette@list.ru>
2020-04-07 02:42:55 +0200

    done with numbers and var sum

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,14 @@
-// Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var message = "Number of students: " + numberOfStudents;
+console.log(message);
+
+var numberOfMentors = 8;
+var message = "Number of mentors: " + numberOfMentors;
+console.log(message);
+
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var sum = numberOfStudents + numberOfMentors;
+
+var message = "Total number of students and mentors: " + sum;
+console.log(message);
