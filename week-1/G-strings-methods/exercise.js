fcdab057b3b9e13c4a26486e0af3fcad14090466
Viktoryia802 <villette@list.ru>
2020-04-07 02:12:52 +0200

    completed with name length 1

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,6 @@
-// Start by creating a variable `message`
+var name = "Viktoryia";
+var lengthName = name.length;
+var message =
+  "My name is Viktoryia and my name is " + lengthName + " characters long";
 
 console.log(message);
