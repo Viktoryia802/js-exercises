cface7d8ae6144d141f8186f0d7fa64f035a9347
Viktoryia802 <villette@list.ru>
2020-04-07 03:13:06 +0200

    done but is not in %, need help

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var percent = (numberOfStudents * 100) / 23;
+var percent = (numberOfMentors * 100) / 23;
+
+var message = "Percentage students: " + percent;
+var message = "Percentage mentors: " + percent;
+
+console.log(message);
